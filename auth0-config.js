ad20caa5940417330db68e589e2414d596afa8b0
am-90 <mauriziantonio90@gmail.com>
2025-07-01 15:14:59 +0200

    Add files via upload

---
+++ auth0-config.js
@@ -0,0 +1,6 @@
+// Replace placeholders with your Auth0 credentials
+window.authConfig = {
+  domain: 'dev-qs5puae3j086rctr.us.auth0.com',
+  clientId: 'eIBQLMkCcUPFPpp7ebmRRJjc8O8C8sgW',
+  audience: 'YOUR_AUTH0_AUDIENCE' // optional, if you use API
+};
