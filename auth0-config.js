6a1ac558bcd822cbf5e6d43e348200f2aa7ffb80
am-90 <mauriziantonio90@gmail.com>
2025-07-01 12:34:35 +0200

    Update auth0-config.js

--- auth0-config.js
+++ auth0-config.js
@@ -1,6 +1,6 @@
 // Replace placeholders with your Auth0 credentials
 window.authConfig = {
-  domain: 'YOUR_AUTH0_DOMAIN',
-  clientId: 'YOUR_AUTH0_CLIENT_ID',
+  domain: 'dev-qs5puae3j086rctr.us.auth0.com',
+  clientId: 'eIBQLMkCcUPFPpp7ebmRRJjc8O8C8sgW',
   audience: 'YOUR_AUTH0_AUDIENCE' // optional, if you use API
 };
